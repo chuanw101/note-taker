088d5d0be5bdbea1d81172103027ee8f126fbf97
Chuan Wang <chuan.wang101@gmail.com>
2022-04-26 11:51:44 -0700

    create post notes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 const path = require('path');
+const fs = require('fs');
 
 const PORT = 3000;
 const notes = require('./db/db.json');
@@ -19,6 +21,29 @@ app.get('/api/notes', (req, res) => {
     // Log our request to the terminal
     console.info(`${req.method} request received to get notes`);
 
+    // Sending all notes to the client
+    return res.json(notes);
+});
+
+// POST request to add a note
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+    
+    // add unique id to note
+    const id = uuidv4();
+    req.body.id = id;
+    notes.push(req.body);
+
+    // stringify but keep formatting
+    const content = JSON.stringify(notes, null, "    ");
+
+    // write to db.json
+    fs.writeFile('./db/db.json', content, function (err) {
+        if (err) throw err;
+        console.log('Saved to ./db/db.json!');
+      });
+    
     // Sending all notes to the client
     return res.json(notes);
 });
