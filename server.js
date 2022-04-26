55389d88b2f791541450fc7e9819c3ea216af43e
Chuan Wang <chuan.wang101@gmail.com>
2022-04-26 12:11:03 -0700

    use statics

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ const app = express();
 // Middleware for parsing application/json and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.use(express.static('public'));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
